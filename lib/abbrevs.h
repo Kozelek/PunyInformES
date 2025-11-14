072c3f63f4bb7bb6a3a36daed0d4c503e3dc61d5
Pablo Maria Martinez Merino <pablote2es@gmail.com>
2025-11-14 23:59:54 +0100

    v2.1.0 Corrige library_of_horror.inf

--- lib/abbrevs.h
+++ lib/abbrevs.h
@@ -2,63 +2,63 @@ Abbreviate "referirte a ";                  !     2x12, saved     8
 Abbreviate "inesperado.";                   !     3x12, saved    18
 Abbreviate "[Comentario";                   !     2x15, saved    11
 Abbreviate "PunyInform";                    !     3x12, saved    18
-Abbreviate "No puedes ";                    !    11x11, saved    87
 Abbreviate "Por favor,";                    !     2x12, saved     8
+Abbreviate "encuentras";                    !     3x10, saved    12
+Abbreviate "No puedes ";                    !    11x11, saved    87
+Abbreviate "e refiere";                     !     3x 9, saved    12
 Abbreviate "ste juego";                     !     4x 9, saved    19
 Abbreviate "cripcione";                     !     3x 9, saved    12
-Abbreviate "encuentra";                     !     3x 9, saved    12
-Abbreviate "e refiere";                     !     3x 9, saved    12
-Abbreviate "irection";                      !     4x 8, saved    15
 Abbreviate "Messages";                      !     2x 9, saved     5
+Abbreviate "irection";                      !     4x 8, saved    15
+Abbreviate "entiendo";                      !     2x 8, saved     3
 Abbreviate "especial";                      !     3x 8, saved     9
-Abbreviate "No hay ";                       !     6x 8, saved    27
 Abbreviate "LookMod";                       !     3x 9, saved    12
-Abbreviate " mismo";                        !     5x 6, saved    14
-Abbreviate "ocurre";                        !     3x 6, saved     6
-Abbreviate "direcc";                        !     4x 6, saved    10
-Abbreviate " nada ";                        !    11x 6, saved    38
-Abbreviate "/when_";                        !     4x 8, saved    15
-Abbreviate "sobre ";                        !     5x 6, saved    14
+Abbreviate "No hay ";                       !     6x 8, saved    27
 Abbreviate "rimero";                        !     6x 6, saved    18
-Abbreviate "epetir";                        !     3x 6, saved     6
-Abbreviate "lmente";                        !     4x 6, saved    10
+Abbreviate "sobre ";                        !     5x 6, saved    14
 Abbreviate "ntro d";                        !     4x 6, saved    10
+Abbreviate "/when_";                        !     4x 8, saved    15
+Abbreviate " mismo";                        !     5x 6, saved    14
+Abbreviate "parece";                        !     4x 6, saved    10
+Abbreviate "direcc";                        !     4x 6, saved    10
+Abbreviate "ocurre";                        !     3x 6, saved     6
+Abbreviate "epetir";                        !     3x 6, saved     6
+Abbreviate " nada ";                        !    11x 6, saved    38
 Abbreviate " cerr";                         !    10x 5, saved    24
-Abbreviate "Pero ";                         !     6x 6, saved    18
-Abbreviate "ando ";                         !     6x 5, saved    12
 Abbreviate " pued";                         !    10x 5, saved    24
 Abbreviate "levas";                         !     7x 5, saved    15
-Abbreviate "eact_";                         !     2x 6, saved     2
+Abbreviate "ando ";                         !     6x 5, saved    12
 Abbreviate " que ";                         !    16x 5, saved    42
-Abbreviate " en ";                          !    20x 4, saved    34
-Abbreviate "Sólo";                          !     3x 8, saved     9
-Abbreviate " ***";                          !     2x13, saved     7
-Abbreviate "able";                          !     7x 4, saved     8
+Abbreviate "eact_";                         !     2x 6, saved     2
+Abbreviate "Pero ";                         !     6x 6, saved    18
 Abbreviate "_to/";                          !    11x 6, saved    38
-Abbreviate "ería";                          !     5x 7, saved    16
 Abbreviate "nada";                          !     8x 4, saved    10
+Abbreviate "able";                          !     7x 4, saved     8
+Abbreviate "ería";                          !     5x 7, saved    16
+Abbreviate " en ";                          !    20x 4, saved    34
+Abbreviate " ***";                          !     2x13, saved     7
+Abbreviate "Sólo";                          !     3x 8, saved     9
 Abbreviate " no ";                          !    16x 4, saved    26
-Abbreviate "es ";                           !    22x 3, saved    19
-Abbreviate "ent";                           !    19x 3, saved    16
 Abbreviate "ión";                           !     7x 6, saved    22
+Abbreviate "es ";                           !    22x 3, saved    19
+Abbreviate "No ";                           !    27x 4, saved    48
 Abbreviate " de";                           !    27x 3, saved    24
 Abbreviate "as ";                           !    27x 3, saved    24
-Abbreviate "stá";                           !    24x 6, saved    90
+Abbreviate "par";                           !     8x 3, saved     5
+Abbreviate "er ";                           !    15x 3, saved    12
 Abbreviate " co";                           !    23x 3, saved    20
 Abbreviate "ien";                           !    15x 3, saved    12
 Abbreviate "est";                           !    15x 3, saved    12
-Abbreviate "No ";                           !    27x 4, saved    48
 Abbreviate "a e";                           !    10x 3, saved     7
 Abbreviate "bje";                           !     7x 3, saved     4
-Abbreviate "par";                           !    12x 3, saved     9
 Abbreviate "per";                           !    10x 3, saved     7
-Abbreviate " si";                           !     9x 3, saved     6
-Abbreviate "er ";                           !    15x 3, saved    12
+Abbreviate "nte";                           !    18x 3, saved    15
+Abbreviate " si";                           !     8x 3, saved     5
+Abbreviate "stá";                           !    24x 6, saved    90
 Abbreviate "a.";                            !    11x 3, saved     8
 Abbreviate "o.";                            !    21x 3, saved    18
 Abbreviate "]^";                            !     4x 6, saved    10
 Abbreviate ", ";                            !     9x 3, saved     6
+Abbreviate "é";                             !     9x 4, saved    12
 Abbreviate ">";                             !     6x 4, saved     6
 Abbreviate "í";                             !    27x 4, saved    48
-Abbreviate "ú";                             !     6x 4, saved     6
-Abbreviate "é";                             !     9x 4, saved    12
